1e986cde3bb858aa12172ef30352e81ea175c4b3
yangje.park <yangje.park@linecorp.com>
2019-01-14 16:42:10 +0900

    setting a project

---
+++ RxCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxCamera
+//
+//  Created by LinePlus on 2019. 1. 14..
+//  Copyright © 2019년 yangjehpark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
